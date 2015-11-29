8fb86103bb564763fb4666d2ebd95171e38cc856
cinada <clark.inada@gmail.com>
2015-11-29 02:42:56 -0800

    testing 1

--- node/deploy_server.js
+++ node/deploy_server.js
@@ -2,6 +2,7 @@ require('./nodeconfig.js');
 require('shelljs/global');
 var io = require('socket.io')(SOCKET_PORT);
 console.log('socket start on port: ' + SOCKET_PORT);
+io.set('origins','*:*');
 io.on('connection', function (socket) {
     console.log('connection made')
     var actions = {
