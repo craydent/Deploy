eeef77f742f08b169e2d3954446eeed24614a4f3
clarkinada <clark.inada@hart.com>
2016-03-01 00:30:46 -0800

    point to correct location for scripts

--- shell_scripts/node_script.sh
+++ shell_scripts/node_script.sh
@@ -9,11 +9,11 @@ sudo mkdir -p /var/craydentdeploy/git/$1;
 cd /var/craydentdeploy/git/$1;
 process_list=(${2});
 list=$(echo ${process_list[@]}|tr " " "|")
-echo $list;
 #kill node processes in process_list
 sudo ps aux | egrep "$list".*|awk '{print $2}' | xargs kill -9
 
 if [ -z "$4" ]; then
+echo "/var/craydentdeploy/git/$1/$3/";
     logBasePath="/var/craydentdeploy/log/$1";
     sudo mkdir -p "$logBasePath/archive";
 
