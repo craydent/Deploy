89d90095d02b4c6206ac96849e052a8db75085c3
clarkinada <clark.inada@hart.com>
2016-03-01 13:23:22 -0800

    attempt to set user to run as

--- shell_scripts/node_script.sh
+++ shell_scripts/node_script.sh
@@ -7,13 +7,14 @@
 
 #sudo mkdir -p /var/craydentdeploy/git/$1;
 #cd /var/craydentdeploy/git/$1;
+gitdir='/var/craydentdeploy/git';
 process_list=(${2});
 list=$(echo ${process_list[@]}|tr " " "|")
 #kill node processes in process_list
 echo "terminating process $1";
 echo "before kill $1 $2 $3 $4 \"$list\"";
 ps aux | egrep "$list".*;
-ps aux | egrep "$list".*|awk '{print $2}' | xargs kill -9
+ps aux | egrep "node\s$gitdir/$1/$3/($list)$".*|awk '{print $2}' | xargs kill -9
 echo "$4 parameter4";
 echo "after kill $1 $2 $3 $4";
 if [ -z "$4" ]; then
@@ -23,7 +24,7 @@ echo "/var/craydentdeploy/git/$1/$3/";
 
     for i in "${process_list[@]}"; do
         cp $logBasePath/$i.log "$logBasePath/archive/$i.log.$(date +%F_%R)" &
-        nohup node /var/craydentdeploy/git/$1/$3/$i > "$logBasePath/$i.log" &
+        nohup node $gitdir/$1/$3/$i > "$logBasePath/$i.log" &
     done
 
     ps aux | egrep "$list".*;
