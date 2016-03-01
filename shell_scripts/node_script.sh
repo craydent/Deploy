6159dceba09c2473290e66cdc5efde22853cbf81
clarkinada <clark.inada@hart.com>
2016-03-01 12:44:42 -0800

    attempt to set user to run as

--- shell_scripts/node_script.sh
+++ shell_scripts/node_script.sh
@@ -11,6 +11,7 @@ process_list=(${2});
 list=$(echo ${process_list[@]}|tr " " "|")
 #kill node processes in process_list
 echo "terminating process $1";
+echo "before kill $1 $2 $3 $4";
 ps aux | egrep "$list".*|awk '{print $2}' | xargs kill -9
 echo "$4 parameter4";
 echo "after kill $1 $2 $3 $4";
