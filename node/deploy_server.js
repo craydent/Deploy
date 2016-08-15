41142372b7a7cb037e9c9040888ef2dc27574549
clarkinada <clark.inada@hart.com>
2016-08-14 22:41:32 -0500

    fix yield keyword issue increment version

--- node/deploy_server.js
+++ node/deploy_server.js
@@ -1,5 +1,5 @@
 /*/---------------------------------------------------------/*/
-/*/ Craydent LLC deploy-v0.1.5                              /*/
+/*/ Craydent LLC deploy-v0.1.6                              /*/
 /*/ Copyright 2011 (http://craydent.com/about)              /*/
 /*/ Dual licensed under the MIT or GPL Version 2 licenses.  /*/
 /*/ (http://craydent.com/license)                           /*/
