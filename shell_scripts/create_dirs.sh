cdcfeea6d527a2febfbd4a4f5a5780d88f7bc0dc
clarkinada <clark.inada@hart.com>
2016-09-15 22:42:20 -0700

    update for no conflict

--- shell_scripts/create_dirs.sh
+++ shell_scripts/create_dirs.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #/*/---------------------------------------------------------/*/
-#/*/ Craydent LLC deploy-v0.1.28                             /*/
+#/*/ Craydent LLC deploy-v0.2.0                              /*/
 #/*/ Copyright 2011 (http://craydent.com/about)              /*/
 #/*/ Dual licensed under the MIT or GPL Version 2 licenses.  /*/
 #/*/ (http://craydent.com/license)                           /*/
